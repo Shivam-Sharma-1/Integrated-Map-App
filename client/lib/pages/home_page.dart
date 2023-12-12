626fa49940af4474ce068a7f12b1291b9a57860a
Shivam77607 <shivamsharma77607@gmail.com>
2023-12-12 19:05:32 +0530

    feat: Add app bar and user list to home page

--- client/lib/pages/home_page.dart
+++ client/lib/pages/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:map_integration/components/post_item.dart';
+import 'package:map_integration/styles/app_colors.dart';
+import 'package:map_integration/styles/app_text.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello");
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.background,
+          title: const Text("%minute flutter"),
+          centerTitle: false,
+          actions: const [Icon(Icons.location_on_outlined)],
+        ),
+        body: ListView(
+          children: mockUsersFromServer(),
+        ));
   }
+}
+
+List<Widget> mockUsersFromServer() {
+  List<Widget> users = [];
+  for (var i = 0; i < 10000; i++) {
+    users.add(PostItem());
+  }
+
+  return users;
 }
