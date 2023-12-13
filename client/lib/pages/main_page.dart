0520f2e5f4e0f869fc1e7165958112b18201a399
Shivam77607 <shivamsharma77607@gmail.com>
2023-12-13 21:21:17 +0530

    feat: add bottom navigation bar in MainPage

--- client/lib/pages/main_page.dart
+++ client/lib/pages/main_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:map_integration/pages/home_page.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../components/bottom_navigation_item.dart';
+import '../config/app_icons.dart';
+import '../styles/app_colors.dart';
+import 'home_page.dart';
 import 'profile_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -12,47 +15,126 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int currentIndex = 0;
-
+  Menus currentIndex = Menus.home;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/ic_home.svg"), label: "Home"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/ic_favorite.svg"),
-              label: "Favorite"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/ic_add.svg"),
-              label: "Add Post"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/ic_messages.svg"),
-              label: "Messages"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/ic_user.svg"), label: "User"),
-        ],
-        onTap: (index) {
+      extendBody: true,
+      body: pages[currentIndex.index],
+      bottomNavigationBar: MyBottomNavigation(
+        currentIndex: currentIndex,
+        onTap: (value) {
           setState(() {
-            currentIndex = index;
+            currentIndex = value;
           });
         },
-        type: BottomNavigationBarType.fixed,
-        currentIndex: currentIndex,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: Colors.amber,
       ),
     );
   }
 
   final pages = [
     HomePage(),
-    Center(child: Text("Favorite")),
-    Center(child: Text("Add Post")),
-    Center(child: Text("Messages")),
-    ProfilePage(),
+    Center(
+      child: Text('Favorite'),
+    ),
+    Center(
+      child: Text('Add Post'),
+    ),
+    Center(
+      child: Text('Messages'),
+    ),
+    ProfilePage()
   ];
+}
+
+enum Menus {
+  home,
+  favorite,
+  add,
+  messages,
+  user,
+}
+
+class MyBottomNavigation extends StatelessWidget {
+  final Menus currentIndex;
+  final ValueChanged<Menus> onTap;
+  const MyBottomNavigation({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 87,
+      margin: EdgeInsets.all(24),
+      child: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            left: 0,
+            top: 17,
+            child: Container(
+              height: 70,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(25))),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: BottomNavigationItem(
+                        onPressed: () => onTap(Menus.home),
+                        icon: AppIcons.icHome,
+                        current: currentIndex,
+                        name: Menus.home),
+                  ),
+                  Expanded(
+                    child: BottomNavigationItem(
+                        onPressed: () => onTap(Menus.favorite),
+                        icon: AppIcons.icFavorite,
+                        current: currentIndex,
+                        name: Menus.favorite),
+                  ),
+                  Spacer(),
+                  Expanded(
+                    child: BottomNavigationItem(
+                        onPressed: () => onTap(Menus.messages),
+                        icon: AppIcons.icMessage,
+                        current: currentIndex,
+                        name: Menus.messages),
+                  ),
+                  Expanded(
+                    child: BottomNavigationItem(
+                        onPressed: () => onTap(Menus.user),
+                        icon: AppIcons.icUser,
+                        current: currentIndex,
+                        name: Menus.user),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: GestureDetector(
+              onTap: () => onTap(Menus.add),
+              child: Container(
+                width: 64,
+                height: 64,
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  shape: BoxShape.circle,
+                ),
+                child: SvgPicture.asset(AppIcons.icAdd),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
