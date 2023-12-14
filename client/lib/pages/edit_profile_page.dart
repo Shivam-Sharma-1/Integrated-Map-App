c198a79dc163a78563099f2935c70397f117fa28
Shivam77607 <shivamsharma77607@gmail.com>
2023-12-14 19:31:01 +0530

    feat: Add EditProfilePage with user profile editing functionality

---
+++ client/lib/pages/edit_profile_page.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+import '../components/app_text_field.dart';
+import '../components/toolbar.dart';
+import '../components/user_avatar.dart';
+import '../config/app_strings.dart';
+import '../styles/app_colors.dart';
+import '../styles/app_text.dart';
+
+enum Gender { none, male, female, other }
+
+class EditProfilePage extends StatefulWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  var gender = Gender.none;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const Toolbar(title: AppStrings.editProfile),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: UserAvatar(
+                      size: 120,
+                    ),
+                  ),
+                  Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: const BoxDecoration(
+                              color: AppColors.primary,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(6))),
+                          child: const Icon(
+                            Icons.edit,
+                            size: 20,
+                            color: Colors.black,
+                          )))
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const AppTextField(hint: AppStrings.firstName),
+              const SizedBox(
+                height: 16,
+              ),
+              const AppTextField(hint: AppStrings.lastName),
+              const SizedBox(
+                height: 16,
+              ),
+              const AppTextField(hint: AppStrings.phoneNumber),
+              const SizedBox(
+                height: 16,
+              ),
+              const AppTextField(hint: AppStrings.location),
+              const SizedBox(
+                height: 16,
+              ),
+              const AppTextField(hint: AppStrings.birthday),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 12, right: 12, top: 6),
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(12))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppStrings.gender,
+                      style: AppText.body1.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: RadioListTile(
+                            title: const Text(AppStrings.male),
+                            value: Gender.male,
+                            visualDensity: const VisualDensity(
+                                horizontal: VisualDensity.minimumDensity,
+                                vertical: VisualDensity.minimumDensity),
+                            contentPadding: EdgeInsets.zero,
+                            groupValue: gender,
+                            onChanged: (value) {
+                              setState(() {
+                                gender = Gender.male;
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RadioListTile(
+                            visualDensity: const VisualDensity(
+                                horizontal: VisualDensity.minimumDensity,
+                                vertical: VisualDensity.minimumDensity),
+                            title: const Text(AppStrings.female),
+                            value: Gender.female,
+                            contentPadding: EdgeInsets.zero,
+                            groupValue: gender,
+                            onChanged: (value) {
+                              setState(() {
+                                gender = Gender.female;
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RadioListTile(
+                            visualDensity: const VisualDensity(
+                                horizontal: VisualDensity.minimumDensity,
+                                vertical: VisualDensity.minimumDensity),
+                            title: const Text(AppStrings.other),
+                            value: Gender.other,
+                            contentPadding: EdgeInsets.zero,
+                            groupValue: gender,
+                            onChanged: (value) {
+                              setState(() {
+                                gender = Gender.other;
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
